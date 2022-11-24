e60c15a0a18e037b07beca25232e85196bf840fc
Somnath Shirkule <shirkule.somnath@gmail.com>
2022-11-24 20:51:30 +0530

    added put/delete/post APIs

--- src/controllers/carController.js
+++ src/controllers/carController.js
@@ -1,5 +1,9 @@
-import { addNewCar } from "../services/carService.js"
+import { addNewCar, deleteCarById, fetchAllCars, fetchCarById, updateCarById } from "../services/carService.js"
 
 export const addCarController = (app)=> {
     app.post('/cars', addNewCar);
+    app.get('/cars', fetchAllCars);
+    app.get('/cars/:carId', fetchCarById);
+    app.delete('/cars/:carId', deleteCarById);
+    app.put('/cars/:carId', updateCarById);
 }
--- src/services/carService.js
+++ src/services/carService.js
@@ -10,3 +10,31 @@ export const addNewCar = (req, res)=> {
         else res.send(car);
     });
 }
+
+export const fetchAllCars = (req, res) => {
+    Car.find((error, car) => {
+        if(error) res.status(500).send(error);
+        else res.send(car);
+    });
+}
+
+export const fetchCarById = (req, res) => {
+    Car.findById(req.params.carId, (error, car) =>{
+        if(error) res.status(500).send(error);
+        else res.send(car);
+    });
+}
+
+export const deleteCarById = (req, res) => {
+    Car.findByIdAndDelete(req.params.carId, (error) => {
+        if(error) res.status(500).send(error);
+        else res.send({message: 'deleted successfully'});
+    });
+}
+
+export const updateCarById = (req, res) => {
+    Car.findByIdAndUpdate(req.params.carId, req.body, {new: true}, (error, car) => {
+        if(error) res.status(500).send(error);
+        else res.send(car);
+    });
+}
